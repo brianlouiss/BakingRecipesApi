75bbaac91f9b9ee2e94a3c5ae41a565b1b32248e
brianlouiss <brianlouis88@gmail.com>
2021-09-14 22:35:28 -0700

    intial spring init

---
+++ src/main/java/com/shake/bake/bakeshake/BakeshakeApplication.java
@@ -0,0 +1,13 @@
+package com.shake.bake.bakeshake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeshakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakeshakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shake/bake/bakeshake/BakeshakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shake.bake.bakeshake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeshakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
